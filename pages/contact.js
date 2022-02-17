382bc660d5de1326ea7ff64ae6bdd73e1b981025
smith <oliviamegan11@gmail.com>
2022-02-17 14:50:59 +0100

    works page ui update

--- pages/contact.js
+++ pages/contact.js
@@ -1,32 +1,6 @@
-import Image from "next/image";
 import React from "react";
-import { useState } from "react";
-import styles from "../styles/pages/works.module.scss";
-export default function Contact() {
-  const [selectedImage, setSelectedImage] = useState(null);
 
-  const handleChange = (e) => {};
-  return (
-    <div>
-      {selectedImage ? (
-        <Image
-          alt="image"
-          width={200}
-          height={200}
-          src={URL.createObjectURL(selectedImage)}
-        />
-      ) : (
-        ""
-      )}
-      <input
-        className={styles.input}
-        type="file"
-        name="image"
-        onChange={(e) => {
-          setSelectedImage(e.target.files[0]);
-          console.log(URL.createObjectURL(e.target.files[0]));
-        }}
-      />
-    </div>
-  );
+// import styles from "../styles/pages/works.module.scss";
+export default function Contact() {
+  return <div>Contact Us</div>;
 }
