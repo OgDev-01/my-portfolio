5fa3e8b3d2b7da98f26d401c9ed6a20a3ba7c9a7
smith <oliviamegan11@gmail.com>
2022-02-24 09:56:31 +0100

    about page optimization update

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,14 +1,19 @@
 import MainHeader from "./main-header";
 import dynamic from "next/dynamic";
 const Nav = dynamic(() => import("./nav"));
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ThemeToggle from "../utils/themeToggle";
 
 function Layout(props) {
   const [menuOpen, setMenuOpen] = useState(false);
-
-  const [theme, setTheme] = useState("light");
+  let defaultTheme = "light";
+  const [theme, setTheme] = useState(defaultTheme);
+  useEffect(() => {
+    const daylight = new Date().getHours();
+    if (daylight <= 7 || daylight >= 19) {
+      setTheme("dark");
+    }
+  });
 
   const switchTheme = () => {
     const newTheme = theme === "dark" ? "light" : "dark";
