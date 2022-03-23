d326c74e366e84762657af8d2fab1a2872cdb564
smith <oliviamegan11@gmail.com>
2022-03-23 17:12:56 +0100

    Contact page Implementation done

--- components/utils/socialLink.js
+++ components/utils/socialLink.js
@@ -4,38 +4,38 @@ import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { RiDribbbleLine, RiInstagramFill } from "react-icons/ri";
 import styles from "../../styles/headers/nav.module.scss";
 
-export default function SocialLink() {
-  const socials = [
-    {
-      name: "twitter",
-      url: "https://twitter.com/O_sunday15",
-      icon: <FaTwitter className={styles.tIcon} />,
-    },
-    {
-      name: "github",
-      url: "https://github.com/SunGoldTech",
-      icon: <FaGithub className={styles.gIcon} />,
-    },
-    {
-      name: "linkedin",
-      url: "https://www.linkedin.com/in/ogbonna-sunday-06a86116b/",
-      icon: <FaLinkedinIn className={styles.lIcon} />,
-    },
-    {
-      name: "instagram",
-      url: "https://www.instagram.com/ogbonna1502/",
-      icon: <RiInstagramFill className={styles.iIcon} />,
-    },
-    {
-      name: "dribbble",
-      url: "https://dribbble.com/Oliviamegan11",
-      icon: <RiDribbbleLine className={styles.dIcon} />,
-    },
-  ];
+export const Socials = [
+  {
+    name: "twitter",
+    url: "https://twitter.com/O_sunday15",
+    icon: <FaTwitter className={styles.tIcon} />,
+  },
+  {
+    name: "github",
+    url: "https://github.com/SunGoldTech",
+    icon: <FaGithub className={styles.gIcon} />,
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/ogbonna-sunday-06a86116b/",
+    icon: <FaLinkedinIn className={styles.lIcon} />,
+  },
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/ogbonna1502/",
+    icon: <RiInstagramFill className={styles.iIcon} />,
+  },
+  {
+    name: "dribbble",
+    url: "https://dribbble.com/Oliviamegan11",
+    icon: <RiDribbbleLine className={styles.dIcon} />,
+  },
+];
 
+export default function SocialLink() {
   return (
     <div className={styles.socialWrap}>
-      {socials.map((social) => (
+      {Socials.map((social) => (
         <Link key={social.name} href={social.url} passHref>
           <a title={social.name}>{social.icon}</a>
         </Link>
--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,28 @@
-import React from "react";
-
-// import styles from "../styles/pages/works.module.scss";
+import Button from "../components/utils/Button";
+import styles from "../styles/pages/contact.module.scss";
+import Link from "next/link";
+import { Socials } from "../components/utils/socialLink";
 export default function Contact() {
-  return <div>Contact Us</div>;
+  console.log(Socials);
+  return (
+    <div className="aboutRoot">
+      <div className={styles.aboutWrap}>
+        <h1 className={styles.heading}>CONTACT</h1>
+        <p className={styles.description}>Wanna Reach out to me??</p>
+
+        <Link href="mailto:oliviamegan11@gmail.com" passHref>
+          <a className={styles.button}>Drop a Message</a>
+        </Link>
+
+        <small className={styles.or}>||</small>
+        <div className={styles.socialWrap}>
+          {Socials.map((link) => (
+            <Link href={link.url} passHref key={link.url} title={link.name}>
+              <a> {link.icon}</a>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
